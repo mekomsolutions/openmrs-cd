2fd9f6af9ac0860f9a716f511c821a459606c0aa
rbuisson <romain@mekomsolutions.com>
2017-11-08 13:58:53 +0100

    INFRA-96: Configure the Webhook job to trigger the appropriate pipeline
    	modified:   docker/Dockerfile
    	modified:   docker/plugins.txt
    	new file:   docker/webhook_triggers.json
    	modified:   jenkins_home/jenkins_home/jobs/Webhook/config.xml
    	modified:   jenkins_home/jenkins_home/jobs/pipeline1/config.xml
    	modified:   resources/src/pipeline1/impl/bahmniconfig.js
    	new file:   resources/src/webhook/trigger.js

---
+++ resources/src/webhook/trigger.js
@@ -0,0 +1,30 @@
+/**
+* @author Romain Buisson (romain@mekomsolutions.com)
+*
+*/
+var fs = require('fs');
+var utils = require('../utils/utils')
+
+var metadata = JSON.parse(fs.readFileSync("/tmp/metadata.json", 'utf8'))
+
+var triggers = JSON.parse(fs.readFileSync("/usr/share/jenkins/webhook_triggers.json", 'utf8'))
+
+var project = {}
+
+for (var trigger in triggers) {
+  if (triggers.hasOwnProperty(trigger)) {
+    if (metadata.type.indexOf(triggers[trigger])) {
+      project = {
+        "project": trigger
+      }
+    }
+  }
+}
+
+// Export the project as environment variable
+fs.writeFileSync(process.env.WORKSPACE + "/trigger.env", utils.convertToEnvVar(project))
+fs.writeFileSync(process.env.WORKSPACE + "/trigger.json", JSON.stringify(project))
+
+console.log(utils.convertToEnvVar(project))
+
+
