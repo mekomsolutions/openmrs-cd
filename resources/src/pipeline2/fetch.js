cc4051270c799fee2d5b7221172662db3013b1a3
rbuisson <romain@mekomsolutions.com>
2017-11-16 16:12:25 +0100

    INFRA-100: Add the actual pipeline2 job
    	new file:   jenkins_home/jenkins_home/jobs/pipeline2/config.xml

--- resources/src/pipeline2/fetch.js
+++ resources/src/pipeline2/fetch.js
@@ -6,8 +6,14 @@ var fs = require('fs');
 var utils = require('../utils/utils')
 
 var descriptorService = require(__dirname +'/descriptorService')
+var servers = {}
 
-var servers = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
+try {
+  servers = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
+} catch (err) {
+  console.log("[ERROR] No list of existing servers could be fetched.")
+  console.log(err)
+}
 
 descriptorService.fetchRemoteDistroDescriptors(servers, function (errors, result) {
   if (errors.length != 0) {
