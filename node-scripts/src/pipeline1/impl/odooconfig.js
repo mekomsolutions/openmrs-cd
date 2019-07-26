f250811497290f76029ca6cb356ac0804c2c2fcd
rbuisson <romain@mekomsolutions.com>
2019-07-26 18:49:53 +0200

    HSC-5: Support 'odooconfig' project type

---
+++ node-scripts/src/pipeline1/impl/odooconfig.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const model = require("../../utils/model");
+const utils = require("../../utils/utils");
+
+const cmns = require("../commons");
+
+const thisType = "odooconfig";
+
+module.exports = {
+  getInstance: function() {
+    var projectBuild = new model.ProjectBuild();
+
+    projectBuild.getBuildScript = function() {
+      return cmns.getMavenProjectBuildScript(thisType);
+    };
+
+    projectBuild.getDeployScript = function(artifact) {
+      return cmns.getMavenProjectDeployScript(
+        thisType,
+        "ARTIFACT_UPLOAD_URL_" + thisType
+      );
+    };
+
+    projectBuild.getArtifact = function(args) {
+      return cmns.getMavenProjectArtifact(args.pom, "./target", "zip");
+    };
+
+    projectBuild.postBuildActions = function(args) {
+      cmns.mavenPostBuildActions(
+        args.pom.groupId,
+        args.artifactsIds,
+        args.pom.version
+      );
+    };
+
+    return projectBuild;
+  }
+};
