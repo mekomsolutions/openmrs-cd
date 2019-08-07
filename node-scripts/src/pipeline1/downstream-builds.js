e68ad08d84c61468e8da009ca8eef348b5657042
rbuisson <romain@mekomsolutions.com>
2019-08-07 18:29:51 +0200

    INFRA-217: downstream-build.js to support Bahmni Apps artifact

--- node-scripts/src/pipeline1/downstream-builds.js
+++ node-scripts/src/pipeline1/downstream-builds.js
@@ -13,13 +13,52 @@ const config = require(cst.CONFIGPATH);
 const utils = require("../utils/utils");
 const cmns = require("./commons");
 
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const log = require("npmlog");
+
 //
 //  Running the post build routine specific to the current project type
 //
 var projectBuild = require("./impl/" +
   process.env[config.varProjectType()]).getInstance();
 
+//
+//  Reading the list of artifact IDs extracted from the POMs in Groovy (see .jenkinsfile)
+//
+var artifactsIds = [];
+try {
+  // artifacts IDs of all the Maven (sub)modules found, so one per POM found
+  artifactsIds = fs
+    .readFileSync(config.getArtifactIdListFilePath(), "utf-8")
+    .toString()
+    .split("\n");
+} catch (err) {
+  log.warn(
+    "",
+    "No artifacts IDs were extracted out of the POM files, is this not a Maven project?"
+  );
+  log.warn("", JSON.stringify(err, null, 2));
+}
+artifactsIds = artifactsIds.filter(function(el) {
+  return el.trim() !== "";
+});
+
+//
+//  Plan B since there is no POM and this is not a Maven project.
+//  Getting the 'pseudo' Maven info that was attached to the built artifact during the build stage.
+//
+var pseudoPom = {};
+if (_.isEmpty(artifactsIds)) {
+  var arty = JSON.parse(
+    fs.readFileSync(config.getBuildArtifactJsonPath(), "utf-8")
+  );
+  pseudoPom = _.pick(arty.mavenProject, ["groupId", "artifactId", "version"]);
+}
+
 projectBuild.postBuildActions({
   pom: utils.getPom(config.getBuildPomPath()),
-  artifactsIds: cmns.getMavenArtifactIds(config.getArtifactIdListFilePath())
+  artifactsIds: artifactsIds,
+  pseudoPom: pseudoPom
 });
